8487a1ec8856fb6b433237a847a8b5402b844aa5
lPassword012 <m2035ausv@gmail.com>
2025-04-28 21:00:54 -0300

    add: implementar el modelo de curso con inscripcion de estudiantes

--- src/com/lPassword012/app/registroEscuela/modelos/Materia.java
+++ src/com/lPassword012/app/registroEscuela/modelos/Materia.java
@@ -1,4 +1,47 @@
 package com.lPassword012.app.registroEscuela.modelos;
 
-public class Materias {
+import java.util.ArrayList;
+import java.util.List;
+
+public class Materia {
+    private static int contadorId = 0;
+    private int id;
+    private String nombre;
+    private Profesor profesor;
+    private List<Alumno> alumnos = new ArrayList<>();
+
+    public Materia(String nombre, Profesor profesor) {
+        this.id = ++contadorId;
+        this.nombre = nombre;
+        this.profesor = profesor;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public Profesor getProfesor() {
+        return profesor;
+    }
+
+    public void agregarAlumno(Alumno alumno) {
+        alumnos.add(alumno);
+    }
+
+    public List<Alumno> getAlumnos() {
+        return alumnos;
+    }
+
+    public int getCantidadAlumnos() {
+        return alumnos.size();
+    }
+
+    public void mostrarDetalles() {
+        System.out.printf("ID: %d | Materia: %s | Profesor: %s | Alumnos: %d%n",
+                id, nombre, profesor.getNombre(), alumnos.size());
+    }
 }
